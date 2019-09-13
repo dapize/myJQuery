8fb4ae47506ad5c81093c8e1647c7341acbf570c
dapize <danielpz@outlook.com>
2019-09-13 00:40:25 -0500

    .data alpha 1

--- myJQuery.js
+++ myJQuery.js
@@ -1,5 +1,23 @@
 (function (document, window) {
+  // Data
+  const data = new Map()
+  window.veamos = data
 
+  // funcions utils
+  const fn = Object.create(null)
+  fn.gral = Object.create(null)
+  fn.gral.random = Object.create(null)
+  fn.gral.random.number = function (length) {
+    let result = '';
+    let init = 1;
+    while (init <= length) {
+      result += Math.floor((Math.random() * 9))
+      init++
+    }
+    return result
+  }
+
+  // Jquery Obj
   function jQueryObj (obj) {
     this[0] = obj;
     this.length = 1;
@@ -7,13 +25,59 @@
 
   const jQprotoObj = jQueryObj.prototype;
 
+  jQprotoObj.jquery = '3.0.0'
+
   jQprotoObj.data = function (keyName, value) {
-    const el0 = this[0]
+    const element0 = this[0]
     let valData
-    if (Element.prototype.isPrototypeOf(el0)) {
-      return el0.dataset[keyName]
+    if (Element.prototype.isPrototypeOf(element0)) {
+      if (value) {
+        valData = this 
+        this.each(function (index, element) {
+          if (data.has(element)) {
+            data.get(element)[keyName] = value;
+          } else {
+            data.set(element, {
+              [keyName] : value
+            });
+          }
+        })
+      } else {
+        if (data.has(element0)) {
+          valData = data.get(element0)[keyName]
+          if (!valData) valData = element0.dataset[keyName]
+        } else {
+          valData = element0.dataset[keyName]
+        }
+      }
+    } else {
+      const preNameProp = 'jQuery' + this.jquery.split('.').join('')
+      const suffNumber = fn.gral.random.number(18)
+      const filter = Object.keys(element0).filter(function (name) {
+        return name.indexOf(preNameProp) >= 0
+      });
+      if (!keyName && !value) {
+        if (filter.length) {
+          valData = filter[0]
+        } else {
+          const objEmpty = Object.create(null)
+          element0[preNameProp + suffNumber] = objEmpty
+          valData = objEmpty
+        }
+      } else {
+        if (value) {
+          valData = this
+         if (filter.length) {
+          element0[filter[0]][keyName] = value;
+         } else {
+          element0[preNameProp + suffNumber] = value
+         }
+        } else {
+          valData = (filter.length) ? element0[filter[0]][keyName] : undefined
+        }
+      }
     }
-    
+    return valData
   }
 
   jQprotoObj.prop = function (keyName, value) {
@@ -24,6 +88,7 @@
   }
 
 
+  // Jquery Nodes
   function jQuery (selector, context) {
     const _this = this
     if (selector instanceof arguments.callee) { // si se pasa un objecto jquery
@@ -47,28 +112,31 @@
     }
   }
 
-  const jQproto = jQuery.prototype;
+  const jQproto = jQuery.prototype = Object.create(jQprotoObj)
 
+  jQproto.html = function (html) {
+    let _this = this;
+    if (html) {
+      this.each(function (element) {
+        element.innerHTML = html;
+      })
+    } else {
+      _this = this[0].innerHTML;
+    }
+    return _this;
+  };
 
-
-  // Utils
-  jQproto._map = function (nodeElements) {
-    return Array.prototype.map.call(nodeElements, function (node) {
-      return node
-    })
+  jQproto.each = function (fn) {
+    let i = 0;
+    while(i < this.length) {
+      fn.call(this[i], i, this[i])
+      i++
+    }
   }
 
+  // Export jQuery
   window.$ = window.jQuery = function (selector, context) {
     return (Object.prototype.toString.call(selector) === '[object Object]') ? new jQueryObj(selector) : new jQuery(selector, context)
   }
 
 }(document, window));  
-console.time('comienza');
-
-
-const elements = $(window);
-console.log(elements);
-
-
-console.timeEnd('comienza');
-
