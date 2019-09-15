f61dc79bd155f76ba0469183ba0b796819cb1ff6
dapize <danielpz@outlook.com>
2019-09-15 15:22:48 -0500

    feat(Selector): Create DOM Elements

--- myJQuery.js
+++ myJQuery.js
@@ -1,14 +1,13 @@
 (function (document, window) {
   // Data
   const data = new Map()
-  window.veamos = data
 
   // funcions utils
   const fn = Object.create(null)
-  fn.gral = Object.create(null)
-  fn.gral.random = Object.create(null)
-  fn.gral.random.number = function (length) {
-    let result = '';
+
+  fn.random = Object.create(null)
+  fn.random.number = function (length) {
+    let result = ''
     let init = 1;
     while (init <= length) {
       result += Math.floor((Math.random() * 9))
@@ -17,15 +16,33 @@
     return result
   }
 
+  fn.regex = Object.create(null)
+  fn.regex.tag = /<[^>]+>/g
+  
+  fn.obj = Object.create(null)
+  fn.obj.isLiteral = function (obj) {
+    return Object.prototype.toString.call(obj).toLowerCase() === '[object object]'
+  }
+
+  fn.string = Object.create(null)
+  fn.string.isDomString = function (stringHtml) {
+    return fn.regex.tag.test(stringHtml)
+  }
+  fn.string.toNode = function (domString) {
+    const div = document.createElement('div')
+    div.innerHTML = domString
+    return div.childNodes
+  }
+
   // Jquery Obj
   function jQueryObj (obj) {
-    this[0] = obj;
-    this.length = 1;
-  };
+    this[0] = obj
+    this.length = 1
+  }
 
-  const jQprotoObj = jQueryObj.prototype;
+  const jQprotoObj = jQueryObj.prototype
 
-  jQprotoObj.jquery = '3.0.0'
+  jQprotoObj.jquery = '3.0.0',
 
   jQprotoObj.data = function (keyName, value) {
     const element0 = this[0]
@@ -33,13 +50,14 @@
     if (Element.prototype.isPrototypeOf(element0)) {
       if (value) {
         valData = this 
+        let objSet
         this.each(function (index, element) {
           if (data.has(element)) {
-            data.get(element)[keyName] = value;
+            data.get(element)[keyName] = value
           } else {
-            data.set(element, {
-              [keyName] : value
-            });
+            objSet = Object.create(null)
+            objSet[keyName] = value
+            data.set(element, objSet)
           }
         })
       } else {
@@ -52,10 +70,10 @@
       }
     } else {
       const preNameProp = 'jQuery' + this.jquery.split('.').join('')
-      const suffNumber = fn.gral.random.number(18)
+      const suffNumber = fn.random.number(18)
       const filter = Object.keys(element0).filter(function (name) {
         return name.indexOf(preNameProp) >= 0
-      });
+      })
       if (!keyName && !value) {
         if (filter.length) {
           valData = filter[0]
@@ -67,11 +85,11 @@
       } else {
         if (value) {
           valData = this
-         if (filter.length) {
-          element0[filter[0]][keyName] = value;
-         } else {
+          if (filter.length) {
+          element0[filter[0]][keyName] = value
+          } else {
           element0[preNameProp + suffNumber] = value
-         }
+          }
         } else {
           valData = (filter.length) ? element0[filter[0]][keyName] : undefined
         }
@@ -92,8 +110,8 @@
     if (selector) { // se pasó algo, veamos que es...
       let elements
       context = context || document
-      if (typeof selector === 'string') { // ... un selector CSS
-        elements = context.querySelectorAll(selector.trim())
+      if (typeof selector === 'string') { // ... un selector CSS o un DOM String
+        elements = (fn.string.isDomString(selector)) ? fn.string.toNode(selector) : context.querySelectorAll(selector.trim())
       } else { // ... un nodo iterable
         elements = (NodeList.prototype.isPrototypeOf(selector) || HTMLCollection.prototype.isPrototypeOf(selector)) ? selector : [selector]
       }
@@ -108,37 +126,37 @@
   const jQproto = jQuery.prototype = Object.create(jQprotoObj)
 
   jQproto.html = function (html) {
-    let _this = this;
+    let _this = this
     if (html) {
       this.each(function (element) {
-        element.innerHTML = html;
+        element.innerHTML = html
       })
     } else {
-      _this = this[0].innerHTML;
+      _this = this[0].innerHTML
     }
-    return _this;
-  };
+    return _this
+  }
 
-  jQproto.each = function (fn) {
-    let i = 0;
+  jQproto.each = function (cb) {
+    let i = 0
     while(i < this.length) {
-      fn.call(this[i], i, this[i])
+      cb.call(this[i], i, this[i])
       i++
     }
   }
 
   // Export jQuery
   window.$ = window.jQuery = function (selector, context) {
-    let retorno;
+    let retorno
     if (selector instanceof jQuery) {
       retorno = Object.create(selector)
       Object.keys(selector).forEach(function (keyName) {
         retorno[keyName] = selector[keyName]
       })
     } else {
-      retorno = (Object.prototype.toString.call(selector).toLowerCase() === '[object object]') ? new jQueryObj(selector) : new jQuery(selector, context)
+      retorno = fn.obj.isLiteral(selector) ? new jQueryObj(selector) : new jQuery(selector, context)
     }
-    return retorno;
+    return retorno
   }
 
-}(document, window));  
+}(document, window))
