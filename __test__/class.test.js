7e74f2f2181d34ffefa484dc4508cca9ae3c7412
dapize <danielpz@outlook.com>
2019-10-02 21:02:06 -0500

    feat(CSS): add, remove, toggle class

--- __test__/class.test.js
+++ __test__/class.test.js
@@ -17,13 +17,12 @@ describe('.hasClass(), .addClass(), .removeClass()', () => {
   })
 
   test('.addClass()', () => {
-    const htmlLis = `
+    document.body.innerHTML = `
       <ul>
         <li>item 1</li>
         <li>item 2</li>
         <li>item 3</li>
       </ul>`;
-    document.body.innerHTML = htmlLis;
     const lis = document.querySelectorAll('li');
     const $li = $('li');
     expect($li.addClass('item')).toEqual({0: lis[0], 1: lis[1], 2: lis[2], length: 3, prevObject: {0: document, length: 1}})
@@ -39,12 +38,12 @@ describe('.hasClass(), .addClass(), .removeClass()', () => {
   })
 
   test('.removeClass()',  () => {
-    const htmlLis = `
-      <ul class="list">
-        <li class="item item-1">item 1</li>
-        <li class="item item-2">item 2</li>
-        <li class="item item-3">item 3</li>
-      </ul>`;
+    document.body.innerHTML = `
+    <ul class="list">
+      <li class="item item-1">item 1</li>
+      <li class="item item-2">item 2</li>
+      <li class="item item-3">item 3</li>
+    </ul>`;
     $('ul').removeClass('list')
     expect(document.querySelector('ul').classList.contains('list')).toBeFalsy()
 
@@ -57,6 +56,42 @@ describe('.hasClass(), .addClass(), .removeClass()', () => {
       expect(li.classList.contains('item')).toBeFalsy()
       expect(li.classList.contains('item-' + index)).toBeFalsy()
     })
+  })
+
+  test('.toggleClass()', () => {
+    document.body.innerHTML = '<div class="main"></div>';
+    const div = document.querySelector('div');
+    const $div = $(div);
+    $div.toggleClass('main')
+    expect(div.classList.contains('main')).toBeFalsy();
+
+    $div.toggleClass('primero segundo');
+    expect(div.classList.contains('primero')).toBeTruthy()
+    expect(div.classList.contains('segundo')).toBeTruthy()
+
+    $div.toggleClass('primero');
+    expect(div.classList.contains('primero')).toBeFalsy()
+
+    $div.toggleClass(function () {
+      return 'primero'
+    });
+    expect(div.classList.contains('primero')).toBeTruthy()
+
+    $div.toggleClass(function () {
+      return 'primero'
+    }, true);
+    expect(div.classList.contains('primero')).toBeTruthy()
+
+    $div.toggleClass(function () {
+      return 'primero'
+    }, false);
+    expect(div.classList.contains('primero')).toBeFalsy()
+
+    div.classList.add('item')
+    $div.toggleClass(function (index, currentClassName) {
+      return currentClassName + '-' + (index + 1)
+    });
+    expect(div.classList.contains('item-1')).toBeTruthy()
   })
   
 })
